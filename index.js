efd6bb6d31c2efaa8be33dee75b5e8d61fa5b33c
Victor H. T. Filho <victorhugo.tozzo@gmail.com>
2023-03-12 00:12:05 -0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-$("h1").css("color","red");
+//$("h1").css("color","red");
+
+//$("a").attr("href", "https://youtube.com");
+
+//$("h1").click(function () {
+//    $("h1").css("color","purple");
+//});
+
+
+$("button").click(function(){
+    $("h1").slideToggle();
+});
+    
+
+//$("input").keydown(function(event){
+//    $("h1").text(event.key);
+//});
+
+//$(document).keydown(function(event){
+//    $("h1").text(event.key);
+//});
+
+$("h1").on("mouseover", function(){
+    $("h1").slideToggle().animate({opacity: 0.5}); 
+});
